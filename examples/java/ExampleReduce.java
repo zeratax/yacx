d664c67556701d3b5f6cf31837cddfdd7ee682ad
Tom-Deuschle <tom-d.172@hotmail.de>
2019-12-10 00:57:22 +0100

    Rename ExampleBlockReduce to ExampleReduce
    block_reduce typically means reduction within one block whereas this alogrithm is for multiple blocks

--- examples/java/ExampleReduce.java
+++ examples/java/ExampleReduce.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Arrays;
 
-public class ExampleBlockReduce {
+public class ExampleReduce {
     //TODO False Result
     public static void main(String[] args) throws IOException {
         //Load Libary
