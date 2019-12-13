2d1a0425b7c038412cbefabb8393875ea486e950
ZerataX <mail@zera.tax>
2019-12-13 14:42:17 +0100

    KernelArgs fix voArgs

--- src/KernelArgs.cpp
+++ src/KernelArgs.cpp
@@ -22,7 +22,7 @@ float KernelArgs::download() {
 
 const void **KernelArgs::content() {
   m_chArgs.resize(m_args.size());
-  std::transform(m_args.begin(), m_args.end(), m_chArgs.begin(),
+  std::transform(m_args.begin(), m_args.end(), m_voArgs.begin(),
                  [](auto &arg) { return arg.content(); });
   return m_chArgs.data();
 };
