5a78431eb317f5dcf97c1db274f3189baa8ae621
visualJames <40604564+visualJames@users.noreply.github.com>
2019-12-20 13:00:10 +0000

    Deploying to gh-pages from master d0bd4d94ef314b8e6bb9b900035932f2240e8ba7

--- search/all_c.js
+++ search/all_c.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['main_2ehpp',['main.hpp',['../main_8hpp.html',1,'']]],
-  ['major',['major',['../classyacx_1_1_device.html#a8377bb720f299ed0af94fcd9b452d650',1,'yacx::Device']]],
+  ['major_5fversion',['major_version',['../classyacx_1_1_device.html#ac72d60151e4070d118cd0aadd0d7d448',1,'yacx::Device']]],
   ['max_5fblock_5fdim',['max_block_dim',['../classyacx_1_1_device.html#a3d7f11438902e714311b41d01ad24e53',1,'yacx::Device']]],
   ['max_5fgrid_5fdim',['max_grid_dim',['../classyacx_1_1_device.html#ad79c81d07b5c49be6d8cc194a5e649e2',1,'yacx::Device']]],
-  ['minor',['minor',['../classyacx_1_1_device.html#af69c43db1d6ede4f78cb0cc2f6d8cddf',1,'yacx::Device']]]
+  ['minor_5fversion',['minor_version',['../classyacx_1_1_device.html#a5d7a0d5f9c543768323cfc8807d9aa27',1,'yacx::Device']]]
 ];
