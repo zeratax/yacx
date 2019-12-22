34a93a4a0b1708239cb0e31ddbaac69626e27dcd
Lukas <lukassiefkehh@gmail.com>
2019-12-22 22:22:03 +0100

    #63, Implementierung in Scala

--- examples/java/ExampleBlockReduce.java
+++ examples/java/ExampleBlockReduce.java
@@ -2,26 +2,25 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class ExampleBlockReduce {
-    //TODO False Result
     public static void main(String[] args) throws IOException {
         //Load Libary
         Executor.loadLibary();
 
         //Testdata
-        int arraySize = 16;
+        int arraySize = 32;
 
-        final int numThreads = 512;
-        final int numBlocks = Math.min((arraySize + numThreads - 1) / numThreads, 1024);
+        final int numBlocks = 512;
+        final int numThreads = Math.min((arraySize + numBlocks - 1) / numBlocks, 1024);
 
         long[] in = new long[arraySize];
-        for (int i = 0; i < in.length; i++){
-            in[i] = i;
+        for (int i = 1; i <= in.length; i++){
+            in[i-1] = i;
         }
 
         //Initialize Arguments
         LongArg inArg = LongArg.create(in, false);
         LongArg outArg = LongArg.createOutput(arraySize);
-        IntArg nArg = IntArg.create(arraySize);
+        KernelArg nArg = IntArg.createValue(arraySize);
 
         //Load kernelString
         String kernelString = Utils.loadFile("kernels/block_reduce.cu");
@@ -31,16 +30,21 @@ public class ExampleBlockReduce {
         //Create compiled Kernel
         Kernel blockReduceKernel = blockReduce.compile();
 
-        //Compile and launch Kernel
+        //Launch Kernel
+        blockReduceKernel.launch(numThreads, numBlocks, inArg, outArg, nArg);
+        
+        //New Input is Output from previous run
+        inArg = LongArg.create(outArg.asLongArray());
+        //Second launch
         blockReduceKernel.launch(numThreads, numBlocks, inArg, outArg, nArg);
 
         //Get Result
-        long[] out = outArg.asLongArray();
+        long out = outArg.asLongArray()[0];
 
         //Print Result
         System.out.println("\nInput:");
         System.out.println(Arrays.toString(in));
-        System.out.println("\nResult:");
-        System.out.println(Arrays.toString(out));
+        System.out.println("\nResult:   " + out);
+        System.out.println("Expected: " + (arraySize * (arraySize+1))/2);
     }
 }
