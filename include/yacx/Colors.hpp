7c2992932b59a8536c0840cd4539b23ce9f50231
ZerataX <mail@zera.tax>
2020-03-28 04:29:56 +0100

    remove static from colors

--- include/yacx/Colors.hpp
+++ include/yacx/Colors.hpp
@@ -3,11 +3,11 @@
 #include <unistd.h>
 
 namespace yacx {
-static bool gColoredOutput = isatty(STDOUT_FILENO);
-static const char *gColorBrightRed = gColoredOutput ? "\u001b[31;1m" : "";
-static const char *gColorBrightGreen = gColoredOutput ? "\u001b[32;1m" : "";
-static const char *gColorBrightYellow = gColoredOutput ? "\u001b[35;1m" : "";
-static const char *gColorBrightDefault = gColoredOutput ? "\u001b[39;1m" : "";
-static const char *gColorGray = gColoredOutput ? "\u001b[100;1m" : "";
-static const char *gColorReset = gColoredOutput ? "\u001b[0m" : "";
+bool gColoredOutput = isatty(STDOUT_FILENO);
+const char *gColorBrightRed = gColoredOutput ? "\u001b[31;1m" : "";
+const char *gColorBrightGreen = gColoredOutput ? "\u001b[32;1m" : "";
+const char *gColorBrightYellow = gColoredOutput ? "\u001b[35;1m" : "";
+const char *gColorBrightDefault = gColoredOutput ? "\u001b[39;1m" : "";
+const char *gColorGray = gColoredOutput ? "\u001b[100;1m" : "";
+const char *gColorReset = gColoredOutput ? "\u001b[0m" : "";
 } // namespace yacx
