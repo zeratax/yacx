859311be89ec7ff5f5e5ef01a6797a764191c182
Jona Abdinghoff <mail@zera.tax>
2020-04-07 21:41:14 +0200

    Update example_logger.cpp

--- examples/example_logger.cpp
+++ examples/example_logger.cpp
@@ -5,7 +5,6 @@ using yacx::loglevel;
 int main(int argc, char const *const *const argv) {
   yacx::handle_logging_args(argc, argv);
 
-  Logger(loglevel::NONE) << "none message";
   Logger(loglevel::ERROR) << "error message";
   Logger(loglevel::WARNING) << "warning message";
   Logger(loglevel::INFO) << "info message";
