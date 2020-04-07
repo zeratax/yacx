ae5e2006e88a3d33a86f3774c833d81c5b885046
ZerataX <mail@zera.tax>
2020-04-07 16:31:20 +0200

    add example file to test logger colors

---
+++ examples/example_logger.cpp
@@ -0,0 +1,16 @@
+#include "yacx/main.hpp"
+
+using yacx::type_of, yacx::loglevel;
+
+int main(int argc, char const *const *const argv) {
+  yacx::handle_logging_args(argc, argv);
+
+  Logger(loglevel::NONE) << "none message";
+  Logger(loglevel::ERROR) << "error message";
+  Logger(loglevel::WARNING) << "warning message";
+  Logger(loglevel::INFO) << "info message";
+  Logger(loglevel::DEBUG) << "debug message";
+  Logger(loglevel::DEBUG1) << "debug message";
+
+  return 0;
+}
